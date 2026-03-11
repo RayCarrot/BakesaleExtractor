ebdcacba7b29be8beb711b71b208229b64c46772
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-03-11 20:13:11 +0100

    Make output dir arg optional

--- src/BakesaleExtractor/Program.cs
+++ src/BakesaleExtractor/Program.cs
@@ -1,13 +1,29 @@
 ﻿using BinarySerializer;
 
-if (args.Length < 2)
+if (args.Length < 1)
 {
-    Console.WriteLine("Usage: BakesaleExtractor <input_file_or_directory> <output_directory>");
+    Console.WriteLine("Usage: BakesaleExtractor <input_file_or_directory> [optional_output_directory]");
     return;
 }
 
 string input = args[0];
-string output = args[1];
+
+bool inputIsFile = File.Exists(input);
+bool inputIsDir = Directory.Exists(input);
+
+if (!inputIsDir && !inputIsFile)
+{
+    Console.WriteLine($"Input path '{input}' does not exist.");
+    return;
+}
+
+string output;
+if (args.Length > 1)
+    output = args[1];
+else if (inputIsFile)
+    output = Path.GetDirectoryName(input) ?? String.Empty;
+else
+    output = input;
 
 Directory.CreateDirectory(output);
 
@@ -18,7 +34,7 @@ riffSettings.RegisterChunkResolver("fmt ", (s, data, chunkSize, name) =>
 riffSettings.RegisterChunkResolver("wdta", (s, data, chunkSize, name) =>
     s.SerializeObject<RIFF_Chunk_WaveData>((RIFF_Chunk_WaveData)data, x => x.Pre_ChunkSize = chunkSize, name: name));
 
-if (File.Exists(input))
+if (inputIsFile)
 {
     using Context context = new(Path.GetDirectoryName(input) ?? String.Empty, serializerLogger: serializerLogger);
     context.AddSettings(riffSettings);
@@ -28,17 +44,13 @@ if (File.Exists(input))
     if (!success)
         Console.WriteLine("Unknown file format");
 }
-else if (Directory.Exists(input))
+else if (inputIsDir)
 {
     using Context context = new(input, serializerLogger: serializerLogger);
     context.AddSettings(riffSettings);
 
     foreach (string file in Directory.GetFiles(input, "*.*", SearchOption.AllDirectories))
         extractFile(context, Path.GetRelativePath(input, file), output);
-}
-else
-{
-    Console.WriteLine($"Input path '{input}' does not exist.");
 }
 
 static bool extractFile(Context context, string fileName, string output)

91423931592eabd9cce896873617d0cca0d9fe0e
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-03-11 20:07:06 +0100

    Name remaining locale values

--- src/BakesaleExtractor/Locale/LocaleFile.cs
+++ src/BakesaleExtractor/Locale/LocaleFile.cs
@@ -3,30 +3,30 @@ using BinarySerializer;
 
 public class LocaleFile : BinarySerializable
 {
-    public Pointer Data1Offset { get; set; }
-    public int Data1Count { get; set; }
-    public Pointer Data2Offset { get; set; }
-    public int Data2Count { get; set; }
+    public Pointer StringKeyHashesOffset { get; set; }
+    public int StringKeyHashesCount { get; set; }
+    public Pointer KeyHashIndexToStringIndexTableOffset { get; set; }
+    public int KeyHashIndexToStringIndexTableCount { get; set; }
     public Pointer LanguagesOffset { get; set; }
     public int LanguagesCount { get; set; }
 
-    public int[] Data1 { get; set; }
-    public int[] Data2 { get; set; }
+    public int[] StringKeyHashes { get; set; } // Seems to use MurmurHash
+    public int[] KeyHashIndexToStringIndexTable { get; set; }
     public LocaleLanguage[] Languages { get; set; }
 
     public override void SerializeImpl(SerializerObject s)
     {
         // Serialize offsets
-        Data1Offset = s.SerializePointer(Data1Offset, anchor: s.CurrentPointer, name: nameof(Data1Offset));
-        Data1Count = s.Serialize<int>(Data1Count, name: nameof(Data1Count));
-        Data2Offset = s.SerializePointer(Data2Offset, anchor: s.CurrentPointer, name: nameof(Data2Offset));
-        Data2Count = s.Serialize<int>(Data2Count, name: nameof(Data2Count));
+        StringKeyHashesOffset = s.SerializePointer(StringKeyHashesOffset, anchor: s.CurrentPointer, name: nameof(StringKeyHashesOffset));
+        StringKeyHashesCount = s.Serialize<int>(StringKeyHashesCount, name: nameof(StringKeyHashesCount));
+        KeyHashIndexToStringIndexTableOffset = s.SerializePointer(KeyHashIndexToStringIndexTableOffset, anchor: s.CurrentPointer, name: nameof(KeyHashIndexToStringIndexTableOffset));
+        KeyHashIndexToStringIndexTableCount = s.Serialize<int>(KeyHashIndexToStringIndexTableCount, name: nameof(KeyHashIndexToStringIndexTableCount));
         LanguagesOffset = s.SerializePointer(LanguagesOffset, anchor: s.CurrentPointer, name: nameof(LanguagesOffset));
         LanguagesCount = s.Serialize<int>(LanguagesCount, name: nameof(LanguagesCount));
 
         // Serialize data from offset
-        s.DoAt(Data1Offset, () => Data1 = s.SerializeArray<int>(Data1, Data1Count, name: nameof(Data1)));
-        s.DoAt(Data2Offset, () => Data2 = s.SerializeArray<int>(Data2, Data2Count, name: nameof(Data2)));
+        s.DoAt(StringKeyHashesOffset, () => StringKeyHashes = s.SerializeArray<int>(StringKeyHashes, StringKeyHashesCount, name: nameof(StringKeyHashes)));
+        s.DoAt(KeyHashIndexToStringIndexTableOffset, () => KeyHashIndexToStringIndexTable = s.SerializeArray<int>(KeyHashIndexToStringIndexTable, KeyHashIndexToStringIndexTableCount, name: nameof(KeyHashIndexToStringIndexTable)));
         s.DoAt(LanguagesOffset, () => Languages = s.SerializeObjectArray<LocaleLanguage>(Languages, LanguagesCount, name: nameof(Languages)));
     }
 }
